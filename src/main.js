7b6d338ff32766ece509af5707c7e147c386e6b4
unknown <pavel@chernin.ru>
2020-02-14 00:03:54 +0300

    vuex added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import {store} from './store'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
